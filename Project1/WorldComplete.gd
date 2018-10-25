1fd3fb2f2b8e49811146e0e048d23108c7b6840e
Joao "Wäve" Marono <jfnmarono@gmail.com>
2018-10-25 18:17:52 +0100

    New_map Transition

--- Project1/WorldComplete.gd
+++ Project1/WorldComplete.gd
@@ -2,5 +2,5 @@ extends Area2D
 func _physics_process(delta):
 	var bodies = get_overlapping_bodies()
 	for body in bodies:
-		if body.name="Player":
-			get_tree().change_scene("")
+		if body.name == "Player":
+			get_tree().change_scene("MagicCliffSpriteStageOne.tscn")
