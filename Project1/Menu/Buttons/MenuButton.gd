c317ea4f88fd5cd113cbff82f1c5c85bb577890e
Rui Pedro Machado Pires <35375217+Nyd3@users.noreply.github.com>
2018-12-19 14:12:09 +0000

    Update MenuButton.gd

--- Project1/Menu/Buttons/MenuButton.gd
+++ Project1/Menu/Buttons/MenuButton.gd
@@ -6,3 +6,20 @@ export (String) var scene_to_load
 
 func _on_ExitButton_pressed():
 	get_tree().quit()
+	
+#fdgdgf
+#fdgdgf
+
+#fdgdgf
+#fdgdgf
+#fdgdgf
+#fdgdgf
+#fdgdgf
+#fdgdgf
+#fdgdgf
+#fdgdgf
+#fdgdgf
+#fdgdgf
+#fdgdgf
+#fdgdgf
+
